7de54576e4676e6bc6feec124d625a2369c558b9
Chee <66661847+ItsChee@users.noreply.github.com>
2020-12-17 00:47:59 -0700

    Music Box Stuff

--- OriginsMusic.cs
+++ OriginsMusic.cs
@@ -1,11 +1,20 @@
+using OriginsMusic.Tiles.MusicBoxes;
+using Terraria;
 using Terraria.ModLoader;
+using static Origins.Origins;
+using static OriginsMusic.Tiles.MusicBoxes.Music_Box_DC;
+using static OriginsMusic.Tiles.MusicBoxes.Music_Box_DW;
 
 namespace OriginsMusic{
 	public class OriginsMusic : Mod {
-		public override void PostAddRecipes(){
-            //Origins.____ =  GetLegacySoundSlot(SoundType.____, "Sounds/Custom/____");
-            //Origins.____ =  GetSoundSlot(SoundType.Music, "Sounds/Music/____");
-            Origins.Origins.Music.Dusk =  GetSoundSlot(SoundType.Music, "Sounds/Music/Dusk");
+		public override void Load(){
+			if (!Main.dedServ) {
+				Origins.Origins.Music.Dusk = GetSoundSlot(SoundType.Music, "Sounds/Music/Dancing_With_Ghosts");
+				Logger.Info(Origins.Origins.Music.Defiled + "");
+				AddMusicBox(Origins.Origins.Music.Defiled = GetSoundSlot(SoundType.Music, "Sounds/Music/Heart_Of_The_Beast"), ModContent.ItemType<Music_Box_DC_Item>(), ModContent.TileType<Music_Box_DC>());
+				AddMusicBox(Origins.Origins.Music.Defiled = GetSoundSlot(SoundType.Music, "Sounds/Music/Shattered_Topography"), ModContent.ItemType<Music_Box_DW_Item>(), ModContent.TileType<Music_Box_DW>());
+				Logger.Info(Origins.Origins.Music.Defiled + "");
+			}
 		}
 	}
 }
---
+++ Tiles/MusicBoxes/Music_Box_DC.cs
@@ -0,0 +1,44 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
+
+namespace OriginsMusic.Tiles.MusicBoxes
+{
+	internal class Music_Box_DC : ModTile
+	{
+		public override void SetDefaults() {
+			Main.tileFrameImportant[Type] = true;
+			Main.tileObsidianKill[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+			TileObjectData.newTile.Origin = new Point16(0, 1);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.addTile(Type);
+			disableSmartCursor = true;
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Music Box");
+			AddMapEntry(new Color(255, 255, 255), name);
+		}
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+		}
+
+		public class Music_Box_DC_Item : ModItem {
+			public override void SetStaticDefaults() {
+				DisplayName.SetDefault("Music Box (Defiled Caverns)");
+			}
+			public override void SetDefaults() {
+				item.accessory = true;
+			}
+		}
+
+		public override void MouseOver(int i, int j) {
+			Player player = Main.LocalPlayer;
+			player.noThrow = 2;
+			player.showItemIcon = true;
+		}
+	}
+}
