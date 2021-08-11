a872e178d5c6f2399972e2406f5905a1dd08914f
Tyfyter <tyfyter2616@gmail.com>
2021-08-11 17:17:48 -0400

    Moved wav files to buildignored "uncrompressed" directory

--- OriginsMusic.cs
+++ OriginsMusic.cs
@@ -8,7 +8,7 @@ using static OriginsMusic.Tiles.MusicBoxes.Music_Box_DW;
 namespace OriginsMusic{
 	public class OriginsMusic : Mod {
 		public override void Load(){
-			if (!Main.dedServ) {
+			if (Main.netMode!=NetmodeID.Server) {
                 //Origins.Origins is not needed because of the static using statement that imports Origins.Origins
 				Music.Dusk = GetSoundSlot(SoundType.Music, "Sounds/Music/Dancing_With_Ghosts");
 				AddMusicBox(Music.Defiled = GetSoundSlot(SoundType.Music, "Sounds/Music/Shattered_Topography"), ModContent.ItemType<Music_Box_DW_Item>(), ModContent.TileType<Music_Box_DW>());
